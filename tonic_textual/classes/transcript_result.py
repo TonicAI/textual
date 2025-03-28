a8f9dad3dd68b5f4982819cc5a155ade34d6ecb7
Adam Kamor <adam@tonic.ai>
2025-03-28 12:18:52 -0400

    add missing file

---
+++ tonic_textual/classes/transcript_result.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class TranscriptionWord:
+    def __init__(self, start: float, end: float, probability: float, word: str):
+        self.start = start
+        self.end = end
+        self.probability = probability
+        self.text = word
+class TranscriptionSegment:
+    def __init__(self, start: float, end: float, id: int, text: str, words: List[TranscriptionWord]):
+        self.start = start
+        self.end = end
+        self.id = id
+        self.text = text
+        self.words = words
+
+class TranscriptionResult:
+    def __init__(self, text: str, language: str, segments: List[TranscriptionSegment]):
+        self.text = text
+        self.language = language
+        self.segments = segments
