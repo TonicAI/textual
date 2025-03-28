075d34986b06994b4e554dd1a4c3386d89daed21
Adam Kamor <adam@tonic.ai>
2025-03-28 16:40:46 -0400

    round3

--- tonic_textual/helpers/base_helper.py
+++ tonic_textual/helpers/base_helper.py
@@ -86,7 +86,6 @@ class BaseHelper(object):
                     rel_end = line_entity_end - line_start
                     
                     # Create a replacement for this line
-                    # For multi-line entities, always use the full entity text and replacement
                     line_entity = Replacement(
                         rel_start,
                         rel_end,
@@ -179,146 +178,65 @@ class BaseHelper(object):
     """
     @staticmethod
     def get_redacted_lines(redaction_response: RedactionResponse, start_and_ends: List[Tuple[int,int]]) -> List[str]:
-        # For other cases, use a proper text replacement algorithm that works with the redacted text
+        """
+        Creates redacted lines by replacing entities within each line.
+        For multi-line entities, each affected line gets the complete replacement text.
+        """
         original_text = redaction_response.original_text
-        redacted_text = redaction_response.redacted_text
         redacted_lines = []
         
-        # Extract redacted lines directly from the API response 
-        # This is the most reliable source of what the redacted text should be
-        if "\n" in original_text and "\n" in redacted_text:
-            # Simple case: split by newline
-            original_lines = original_text.split("\n")
-            redacted_lines = redacted_text.split("\n")
+        # Process entities and collect replacements for each line
+        replacements_by_line = {}
+        for i in range(len(start_and_ends)):
+            replacements_by_line[i] = []
             
-            # Make sure we have the right number of lines
-            if len(original_lines) == len(start_and_ends) and len(redacted_lines) == len(start_and_ends):
-                return redacted_lines
-        
-        # Map entity replacements for each line
-        # We'll construct a redacted version of each line by applying the appropriate replacements
-        line_replacements = {}
-        for line_idx, (line_start, line_end) in enumerate(start_and_ends):
-            line_replacements[line_idx] = []
-        
-        # For each entity, determine which line(s) it affects
+        # Find which entities affect which lines and at what positions
         for entity in redaction_response.de_identify_results:
             entity_start = entity['start']
             entity_end = entity['end']
             entity_text = entity['text']
-            entity_new_text = entity['new_text']
+            replacement_text = entity['new_text']
             
-            # Is this a multi-line entity?
-            multi_line = False
-            affected_lines = []
-            
+            # Determine which lines this entity spans
             for line_idx, (line_start, line_end) in enumerate(start_and_ends):
+                # If this entity affects this line
                 if entity_start < line_end and entity_end > line_start:
-                    affected_lines.append(line_idx)
+                    # Compute the part of the entity that falls in this line
+                    start_in_line = max(0, entity_start - line_start)
+                    end_in_line = min(line_end - line_start, entity_end - line_start)
                     
-            # If entity spans multiple lines
-            if len(affected_lines) > 1:
-                multi_line = True
-                
-                # Figure out what part of the entity is in each affected line
-                for line_idx in affected_lines:
-                    line_start, line_end = start_and_ends[line_idx]
-                    orig_line = original_text[line_start:line_end]
+                    # Store the replacement details
+                    replacements_by_line[line_idx].append({
+                        'start': start_in_line,  # Position in the line, not the full text
+                        'end': end_in_line,
+                        'replacement': replacement_text  # Always use full replacement
+                    })
                     
-                    # Is this the first line containing the entity?
-                    if line_idx == affected_lines[0]:
-                        # First line: from entity start to end of line
-                        line_entity_start = entity_start - line_start
-                        line_entity_end = len(orig_line)
-                        line_entity_text = orig_line[line_entity_start:]
-                        
-                        # Replace this part with the full replacement
-                        line_replacements[line_idx].append({
-                            'start': line_entity_start,
-                            'end': line_entity_end,
-                            'text': line_entity_text,
-                            'replacement': entity_new_text
-                        })
-                    
-                    # Is this the last line containing the entity?
-                    elif line_idx == affected_lines[-1]:
-                        # Last line: from start of line to entity end
-                        line_entity_start = 0
-                        line_entity_end = entity_end - line_start
-                        line_entity_text = orig_line[:line_entity_end]
-                        
-                        # Replace this part with the full replacement
-                        line_replacements[line_idx].append({
-                            'start': line_entity_start,
-                            'end': line_entity_end,
-                            'text': line_entity_text,
-                            'replacement': entity_new_text
-                        })
-                    
-                    # Middle line (complete replacement)
-                    else:
-                        # Middle line: replace entire line
-                        line_replacements[line_idx].append({
-                            'start': 0,
-                            'end': len(orig_line),
-                            'text': orig_line,
-                            'replacement': entity_new_text
-                        })
-            
-            # Single line entity (simpler case)
-            else:
-                line_idx = affected_lines[0]
-                line_start, line_end = start_and_ends[line_idx]
-                
-                # Get the line text
-                orig_line = original_text[line_start:line_end]
-                
-                # Calculate positions within the line
-                rel_start = entity_start - line_start
-                rel_end = entity_end - line_start
-                
-                # Add replacement info
-                line_replacements[line_idx].append({
-                    'start': rel_start,
-                    'end': rel_end,
-                    'text': entity_text,
-                    'replacement': entity_new_text
-                })
-        
-        # Apply replacements to each line
+        # Process each line
         for line_idx, (line_start, line_end) in enumerate(start_and_ends):
-            # Get original line text
-            orig_line = original_text[line_start:line_end]
+            line_text = original_text[line_start:line_end]
+            line_replacements = sorted(replacements_by_line[line_idx], key=lambda r: r['start'])
             
-            # No replacements for this line
-            if line_idx not in line_replacements or not line_replacements[line_idx]:
-                redacted_lines.append(orig_line)
-                continue
+            # Apply replacements to this line
+            result = []
+            last_end = 0
             
-            # Sort replacements by start position (to handle them in order)
-            replacements = sorted(line_replacements[line_idx], key=lambda x: x['start'])
-            
-            # Apply replacements
-            result = orig_line
-            offset = 0  # Track position changes due to replacements
-            
-            for rep in replacements:
-                start = rep['start']
-                end = rep['end']
-                text = rep['text']
-                replacement = rep['replacement']
+            for rep in line_replacements:
+                # Add text before replacement
+                if rep['start'] > last_end:
+                    result.append(line_text[last_end:rep['start']])
                 
-                # Apply the replacement
-                adjusted_start = start + offset
-                adjusted_end = end + offset
+                # Add replacement text
+                result.append(rep['replacement'])
                 
-                # Replace the text
-                result = result[:adjusted_start] + replacement + result[adjusted_end:]
-                
-                # Update offset for next replacement
-                offset += len(replacement) - (end - start)
+                # Update position
+                last_end = rep['end']
             
-            # Add to redacted lines
-            redacted_lines.append(result)
-        
+            # Add any remaining text
+            if last_end < len(line_text):
+                result.append(line_text[last_end:])
+                
+            # Combine into the redacted line and add to results
+            redacted_lines.append(''.join(result))
+            
         return redacted_lines
