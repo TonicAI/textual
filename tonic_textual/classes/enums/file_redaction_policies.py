cbb056c7f104f394f1e70701ecfccb03473ac1f9
Adam Kamor <adam@tonic.ai>
2025-05-16 18:10:27 -0400

    done

--- tonic_textual/classes/enums/file_redaction_policies.py
+++ tonic_textual/classes/enums/file_redaction_policies.py
@@ -1,26 +1,26 @@
 from enum import Enum
 
 
-class docx_image_policy(Enum):
-    redact = 1
-    ignore = 2
-    remove = 3
+class docx_image_policy(str, Enum):
+    redact = 'Redact'
+    ignore = 'Ignore'
+    remove = 'Remove'
 
 
-class docx_comment_policy(Enum):
-    remove = 1
-    ignore = 2
+class docx_comment_policy(str, Enum):
+    remove = 'Remove'
+    ignore = 'Ignore'
 
 
-class docx_table_policy(Enum):
-    redact = 1
-    remove = 2
+class docx_table_policy(str, Enum):
+    redact = 'Redact'
+    remove = 'Remove'
 
 
-class pdf_signature_policy(Enum):
-    redact = 1
-    ignore = 2
+class pdf_signature_policy(str, Enum):
+    redact = 'Redact'
+    ignore = 'Ignore'
 
-class pdf_synth_mode_policy(Enum):
-    V1 = 1
-    V2 = 2
+class pdf_synth_mode_policy(str, Enum):
+    V1 = 'V1'
+    V2 = 'V2'
