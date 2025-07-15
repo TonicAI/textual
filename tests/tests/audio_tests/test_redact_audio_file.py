01f0661c0e4854f5468f23a7d04275ed6f1509a9
Adam Kamor <adam@tonic.ai>
2025-07-15 14:29:11 -0400

    moving audio file to resources

--- tests/tests/audio_tests/test_redact_audio_file.py
+++ tests/tests/audio_tests/test_redact_audio_file.py
@@ -1,2 +1,6 @@
+from tests.utils.resource_utils import get_resource_path
+
+
 def test_redact_audio_file_does_not_throw(textual_audio):
-    textual_audio.redact_audio_file('tests/tests/files/banking_customer_support.mp3', 'output.mp3')
+    path = get_resource_path('banking_customer_support.mp3')
+    textual_audio.redact_audio_file(path, 'output.mp3')
