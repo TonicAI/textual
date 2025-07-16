aa92cb6362ae57bdac16a2e9ae2e98cfa1654ac9
Adam Kamor <adam@tonic.ai>
2025-07-16 07:51:52 -0400

    skipping tests

--- tests/tests/audio_tests/test_redact_audio_file.py
+++ tests/tests/audio_tests/test_redact_audio_file.py
@@ -1,6 +1,7 @@
+import pytest
 from tests.utils.resource_utils import get_resource_path
 
-
+@pytest.mark.skip(reason="Need to deploy ASR first")
 def test_redact_audio_file_does_not_throw(textual_audio):
     path = get_resource_path('banking_customer_support.mp3')
     textual_audio.redact_audio_file(path, 'output.mp3')
