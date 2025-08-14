68655791dd3d23bc88c4e514c72ff4d6fcdf2041
Kirill Medvedev <kirill@tonic.ai>
2025-08-14 11:25:50 -0500

    updated test to validate error when uploading corrupted PDF file

--- tests/tests/dataset_tests/test_get_dataset_files.py
+++ tests/tests/dataset_tests/test_get_dataset_files.py
@@ -3,6 +3,7 @@ from tonic_textual.redact_api import TextualNer
 import uuid
 import io
 import time
+import pytest, requests
 
 def test_processed_files_get_updated(textual: TextualNer):
     ds_name = str(uuid.uuid4()) + 'test-processed-files'
@@ -25,18 +26,21 @@ def test_processed_files_get_updated(textual: TextualNer):
 def test_processed_failed_files(textual: TextualNer):
     ds_name = str(uuid.uuid4())+'test-processed-files'
     ds = textual.create_dataset(ds_name)
-    #invalid pdf
-    ds.add_file(file_name = 'test.pdf', file = create_file_stream("My name is Adam. Again, my name is adam."))
 
-    counter = 0
-    success = False
-    while counter<60:
-        if len(ds.get_failed_files())==1:
-            success=True
-            break
-        time.sleep(1)
-        counter=counter+1
-    assert success, "file should fail to process"
+    #invalid pdf
+    with pytest.raises(requests.exceptions.HTTPError) as exc:
+        ds.add_file(
+            file_name="test.pdf",
+            file=create_file_stream("My name is Adam. Again, my name is adam.")
+        )
+
+    resp = exc.value.response
+    assert resp is not None, "HTTPError should include a response"
+    assert resp.status_code == 400
+    assert resp.headers.get("Content-Type", "").startswith("text/plain")
+
+    expected = 'Could not find the version header comment at the start of the document.'
+    assert resp.text == expected
 
 def create_file_stream(txt: str) -> io.BytesIO:
     return io.BytesIO(txt.encode('utf-8'))
