b661eff171ccaaed0b4bff2c3bf7b0c4efeef648
ethan-tonic <ephilpott@tonic.ai>
2025-09-03 12:43:56 -0400

    Formatting

--- tests/tests/audio_tests/test_get_transcript.py
+++ tests/tests/audio_tests/test_get_transcript.py
@@ -1,9 +1,10 @@
 import pytest
 from tests.utils.resource_utils import get_resource_path
 
+
 def test_get_transcription(textual_audio):
-    path = get_resource_path('banking_customer_support.mp3')
+    path = get_resource_path("banking_customer_support.mp3")
     transcript = textual_audio.get_audio_transcript(path)
-    assert transcript.language=="english" or transcript.language=="en"
-    assert len(transcript.segments)>0
-    assert len(transcript.text)>100
+    assert transcript.language == "english" or transcript.language == "en"
+    assert len(transcript.segments) > 0
+    assert len(transcript.text) > 100
